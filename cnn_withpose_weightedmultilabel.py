28c006031f9a9e8b8b7728a6240e7b3286800b39
jimmyl02 <jimmyli2002@gmail.com>
2020-11-23 19:38:03 -0800

    Updated model

--- cnn_withpose_weightedmultilabel.py
+++ cnn_withpose_weightedmultilabel.py
@@ -23,13 +23,12 @@ import random
 import datetime
 
 import utils.io as io
-from model.cnn_model import HOCNN
+#from model.cnn_model import HOCNN
+from model.cnn_with_pose import HOPOSECNN
 from datasets import metadata
 from utils.vis_tool import vis_img
 from datasets.hico_constants import HicoConstants
 from datasets.hico_dataset import HicoDataset, collate_fn
-
-from optimizers import adabound
 
 import json
 import cv2
@@ -44,6 +43,7 @@ np.random.seed(21)
 
 # Define data paths
 TRAIN_IMG_PATH = "datasets/hico/images/train2015/"
+TRAIN_POSE_PATH = "datasets/human_pose/train2015/"
 
 # Setup training device
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
@@ -52,25 +52,21 @@ print('training on {}'.format(device))
 # Define arguments
 batch_size = 32
 epochs = 300
-initial_lr = 0.0005
-final_lr = 0.1 # Only for adabound optimizer
-l2_weight_decay = 0.00005
-weighted_loss_alpha = 1 # Boost the weight of positives to help negative dominance
-weighted_loss_beta = 0.9999 # From the class based balance formula
+initial_lr = 0.00001
 
 feat_type = 'fc7'
 data_aug = False
-exp_ver = 'v2_nl2_' + datetime.datetime.now().strftime("%Y-%m-%d_%H-%M")
+exp_ver = 'v4_n_' + datetime.datetime.now().strftime("%Y-%m-%d_%H-%M")
 # w = weighted loss, n = normalized (/255), l2 = l2 norm
-# 0 = adagrad, 1 = adabound
-save_dir = './checkpoints/hico'
-log_dir = './log/hico'
+save_dir = './checkpoints/hicopose'
+log_dir = './log/hicopose'
 save_every = 5
 print_batch_every = 100
 print_epoch_every = 1
 
 # set the cache size [0 means infinite]
 max_img_cache_size = 1 #40000
+max_pose_cache_size = 1 #40000
 
 print('Running experiment ' + exp_ver)
 
@@ -80,15 +76,15 @@ data_const = HicoConstants(feat_type=feat_type)
 train_dataset = HicoDataset(data_const=data_const, subset='train', data_aug=data_aug)
 val_dataset = HicoDataset(data_const=data_const, subset='val', data_aug=False, test=True)
 dataset = {'train': train_dataset, 'val': val_dataset}
-print('Set up dataset variable successfully')
+print('set up dataset variable successfully')
 
 train_dataloader = DataLoader(dataset=dataset['train'], batch_size=batch_size, shuffle=True, collate_fn=collate_fn, drop_last=True)
 val_dataloader = DataLoader(dataset=dataset['val'], batch_size=batch_size, shuffle=True, collate_fn=collate_fn, drop_last=True)
 dataloader = {'train': train_dataloader, 'val': val_dataloader}
-print('Set up dataloader successfully')
+print('set up dataloader successfully')
 
 # Define model
-model = HOCNN().to(device)
+model = HOPOSECNN().to(device)
 
 # Display parameter information
 parameter_num = 0
@@ -96,28 +92,9 @@ for param in model.parameters():
     parameter_num += param.numel()
 print(f'The number of parameters in this model is {parameter_num / 1e6} million')
 
-# Define weighted loss due to unbalanced dataset
-# Implemented class balanced loss from [https://github.com/richardaecn/class-balanced-loss]
-# The average of these weights is 1.0
-'''
-with open('datasets/processed/hico/hoi_cls_count.json') as f:
-    hoi_class_count = json.load(f)
-    hoi_classes = hoi_class_count.keys()
-    samples_per_class = np.zeros((len(hoi_classes), ))
-    for hoi_class in hoi_classes:
-        samples_per_class[int(hoi_class) - 1] = hoi_class_count[hoi_class]
-    effective_num = 1.0 - np.power(weighted_loss_beta, samples_per_class)
-    loss_weights = (1.0 - weighted_loss_beta) / np.array(effective_num)
-    loss_weights = weighted_loss_alpha * loss_weights / np.sum(loss_weights) * len(hoi_classes)
-    loss_weights = torch.tensor(loss_weights).float().to(device)
-'''
-
-# Todo: look at distribution balanced loss from [https://github.com/wutong16/DistributionBalancedLoss]
-  
 # Weighted loss using difference between class sizes
 # Each class pos_weight is num_greatest_class / num_pos
 # It seems like the graients explode even using only the num_greatest
-'''
 with open('datasets/processed/hico/hoi_cls_count.json') as f:
     hoi_class_count = json.load(f)
     hoi_classes = hoi_class_count.keys()
@@ -128,20 +105,16 @@ with open('datasets/processed/hico/hoi_cls_count.json') as f:
         samples_per_class[int(hoi_class) - 1] = hoi_class_count[hoi_class]
     loss_weights = greatest_sample / samples_per_class
     loss_weights = torch.tensor(loss_weights).float().to(device)
-'''
     
 print('Calculated loss weights')
 
 # Define loss function
-criterion = nn.BCEWithLogitsLoss()
-#criterion = nn.BCEWithLogitsLoss(reduction='none')
-#criterion = nn.BCEWithLogitsLoss(pos_weight=loss_weights)
+criterion = nn.BCEWithLogitsLoss(pos_weight = loss_weights)
 #criterion = nn.CrossEntropyLoss()
 
 # Define optimizer
-#optimizer = optim.Adam(model.parameters(), lr=initial_lr, weight_decay=l2_weight_decay)
-optimizer = adabound.AdaBound(model.parameters(), lr=initial_lr, final_lr=final_lr, weight_decay=l2_weight_decay)
-#optimizer = optim.SGD(model.parameters(), lr=initial_lr, momentum=0.9, weight_decay=0)
+optimizer = optim.Adam(model.parameters(), lr=initial_lr, weight_decay=0)
+#optimizer = optim.SGD(model.parameters(), lr=args.lr, momentum=0.9, weight_decay=0)
 
 # Setup visualization
 writer = SummaryWriter(log_dir=log_dir + '/' + exp_ver + '/' + 'epoch_train')
@@ -153,6 +126,8 @@ with open('datasets/processed/hico/anno_list.json') as f:
     
 img_cache = {} # format {key: [human, object, pairwise]}
 img_cache_counter = 0
+pose_img_cache = {}
+pose_img_cache_counter = 0
     
 print('Training has started!')
     
@@ -206,9 +181,37 @@ for epoch in range(epochs):
                         img_cache[src_img_path] = [human_bbox_img, obj_bbox_img, pairwise_bbox_img]
                         img_cache_counter += 1
 
+                # Get pose data [with caching]
+                pose_img_path = TRAIN_POSE_PATH + parsed_img_name + '_keypoints.json'
+                if pose_img_path in pose_img_cache: # Use cache if available
+                    pose_img = pose_img_cache[pose_img_path]
+                else:
+                    with open(pose_img_path) as f:
+                        pose_data = json.load(f)
+
+                    pose_img = np.zeros((src.shape[0], src.shape[1], 1))
+
+                    for person in pose_data['people']:
+                        pose_data_2d = person['pose_keypoints_2d']
+                        prev_point = None
+                        for inner_point in range(0, len(pose_data_2d), 3):
+                            x, y, c = pose_data_2d[inner_point], pose_data_2d[inner_point + 1], pose_data_2d[inner_point + 2]
+                            if c > 0:
+                                pose_img = cv2.circle(pose_img, (int(x), int(y)), 10, (255,0,0), \
+                                                      thickness=-1, lineType=cv2.FILLED)
+                                if prev_point:
+                                    pose_img = cv2.line(pose_img, (int(prev_point[0]), int(prev_point[1])), \
+                                                        (int(x), int(y)), (255, 0, 0), 3)
+                                prev_point = (x, y)
+
+                    # Add to cache if within limits
+                    if not max_pose_cache_size or pose_img_cache_counter < max_pose_cache_size:
+                        pose_img_cache[pose_img_path] = pose_img
+                        pose_img_cache_counter += 1
+
                 '''
-                # Visualization of masks NOTE: Not guarenteed to work outside of jupyter notebook
-                f, axarr = plt.subplots(1,3)
+                # Visualization of masks and pose NOTE: Not guarenteed to work outside of jupyter notebook
+                f, axarr = plt.subplots(1,4)
 
                 human_bbox_rgb = cv2.cvtColor(human_bbox_img, cv2.COLOR_BGR2RGB)
                 axarr[0].imshow(human_bbox_rgb)
@@ -219,6 +222,10 @@ for epoch in range(epochs):
                 pairwise_rgb = cv2.cvtColor(pairwise_bbox_img, cv2.COLOR_BGR2RGB)
                 axarr[2].imshow(pairwise_rgb)
 
+                zeroed_channel = np.zeros((src.shape[0], src.shape[1], 1))
+                stacked_pose_img = np.dstack((pose_img, zeroed_channel, zeroed_channel))
+                axarr[3].imshow(stacked_pose_img)
+
                 plt.show()
                 f.clf()
                 '''
@@ -226,28 +233,34 @@ for epoch in range(epochs):
                 human_bbox_img = cv2.resize(human_bbox_img, (64, 64), interpolation=cv2.INTER_AREA)
                 obj_bbox_img = cv2.resize(obj_bbox_img, (64, 64), interpolation=cv2.INTER_AREA)
                 pairwise_bbox_img = cv2.resize(pairwise_bbox_img, (64, 64), interpolation=cv2.INTER_AREA)
-                
+                pose_img = cv2.resize(pose_img, (64, 64), interpolation=cv2.INTER_AREA)
+
                 # Normalize images by dividing by 255
                 human_bbox_img = human_bbox_img/255
                 obj_bbox_img = obj_bbox_img/255
                 pairwise_bbox_img = pairwise_bbox_img/255
-
+                pose_img = pose_img/255
+                
                 human_bbox_img = torch.from_numpy(human_bbox_img).to(device)
                 obj_bbox_img = torch.from_numpy(obj_bbox_img).to(device)
                 pairwise_bbox_img = torch.from_numpy(pairwise_bbox_img).to(device)
+                pose_img = torch.from_numpy(pose_img).to(device)
 
                 if i == 0:
                     res_human_input = human_bbox_img.unsqueeze(0)
                     res_obj_input = obj_bbox_img.unsqueeze(0)
                     res_pairwise_input = pairwise_bbox_img.unsqueeze(0)
+                    res_pose_input = pose_img.unsqueeze(0)
                 else:
                     res_human_input = torch.cat((res_human_input, human_bbox_img.unsqueeze(0)), dim=0)
                     res_obj_input = torch.cat((res_obj_input, obj_bbox_img.unsqueeze(0)), dim=0)
                     res_pairwise_input = torch.cat((res_pairwise_input, pairwise_bbox_img.unsqueeze(0)), dim=0)
+                    res_pose_input = torch.cat((res_pose_input, pose_img.unsqueeze(0)), dim=0)
 
             res_human_input = res_human_input.permute([0,3,1,2]).float().to(device)
             res_obj_input = res_obj_input.permute([0,3,1,2]).float().to(device)
             res_pairwise_input = res_pairwise_input.permute([0,3,1,2]).float().to(device)
+            res_pose_input = res_pose_input.unsqueeze(3).permute([0,3,1,2]).float().to(device)
             labels = torch.from_numpy(labels).float().to(device)
             
             if phase == 'train':
@@ -256,13 +269,13 @@ for epoch in range(epochs):
                 model.zero_grad()
                 
                 # Forward pass: human, objects, pairwise streams
-                outputs = model.forward(res_human_input, res_obj_input, res_pairwise_input)
+                outputs = model.forward(res_human_input, res_obj_input, res_pairwise_input, res_pose_input)
                 loss = criterion(outputs, labels)
-                #loss = (loss * loss_weights).mean()
                 loss.backward()
                 optimizer.step()
                 
                 preds = torch.argmax(outputs, dim=1)
+                ground_labels = torch.max(labels, 1)[1]
                 for accuracy_iterator in range(len(preds)):
                     ground_labels = torch.nonzero(labels[accuracy_iterator]).squeeze()
                     if preds[accuracy_iterator] in ground_labels:
@@ -272,9 +285,8 @@ for epoch in range(epochs):
                 # Evaluation after train loop
                 model.eval()
                 with torch.no_grad(): # Disable gradients for validation
-                    outputs = model.forward(res_human_input, res_obj_input, res_pairwise_input)
+                    outputs = model.forward(res_human_input, res_obj_input, res_pairwise_input, res_pose_input)
                     loss = criterion(outputs, labels)
-                    #loss = (loss * loss_weights).mean()
                     
                     preds = torch.argmax(outputs, dim=1)
                     for accuracy_iterator in range(len(preds)):
@@ -312,7 +324,7 @@ for epoch in range(epochs):
                     phase, epoch+1, epochs, epoch_loss, epoch_accuracy, (end_time-start_time)))
     
     # Save the model per save_every
-    if epoch_loss<0.000405 or epoch % save_every == (save_every - 1) and epoch >= (10-1):
+    if epoch_loss<0.0405 or epoch % save_every == (save_every - 1) and epoch >= (10-1):
         checkpoint = { 
                         'lr': initial_lr,
                        'b_s': batch_size,
